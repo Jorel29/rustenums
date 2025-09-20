7501b7592ea5204268b3602e9aa6615059643e03
Jorel Huerto <jjhue29@gmail.com>
2025-09-19 22:58:58 -0700

    update

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ enum IpAddr{
     V4(u8,u8,u8,u8),
     V6(String),
 }
-let _home = IpAddr::V4(127,0,0,1);
+static _home = IpAddr::V4(127,0,0,1);
 
 let _loopback = IpAddr::V6(String::from("::1")); 
 
